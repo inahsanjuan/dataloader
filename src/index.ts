a4df6123c1795807b3cf83fe223295c630c59293
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-18 14:39:25 +0800

    refactor: move all files to src

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { DataLoader } from "./data-loader/data-loader.class";
-export { DataLoaderModule } from "./data-loader/data-loader.module";
-export { DataLoaderCacheMap } from "./data-loader/data-loader-cache-map.class";
-export { DataLoaderContext } from "./data-loader/data-loader-context.class";
-export { DataLoaderContextMiddleware } from "./data-loader/data-loader-context.middleware";
-export { DataLoaderMiddlewareModule } from "./data-loader/data-loader-middleware.module";
+export { DataLoader } from "./data-loader.class";
+export { DataLoaderModule } from "./data-loader.module";
+export { DataLoaderCacheMap } from "./data-loader-cache-map.class";
+export { DataLoaderContext } from "./data-loader-context.class";
+export { DataLoaderContextMiddleware } from "./data-loader-context.middleware";
+export { DataLoaderMiddlewareModule } from "./data-loader-middleware.module";
