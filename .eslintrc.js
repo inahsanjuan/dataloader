220b88a50ec57b6b0acc2a58268f63c24734e28f
Char2s <63489409+TheNightmareX@users.noreply.github.com>
2021-12-18 13:06:41 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    jest: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint/eslint-plugin", "simple-import-sort"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", ignoreRestSiblings: true },
+    ],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-explicit-any": ["error", { ignoreRestArgs: true }],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+};
